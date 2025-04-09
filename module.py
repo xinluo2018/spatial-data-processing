35049435478720e3cd0dfede97235424ca8f4a1f
xinluo2018 <xinluo_xin@163.com>
2025-04-09 15:43:24 +0800

    new commit

--- module.py
+++
@@ -1,8 +0,0 @@
-
-def num_higher_(s, s_high=90):
-  num = 0
-  for score in s:
-    # print(score)
-    if score >= s_high:
-      num=num+1
-  return num 
--- pick.py
+++ pick.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np
 
 def pick():
-    path_stud = 'students/students_hometown.csv'
+    path_stud = 'students/students_2025_hometown.csv'
     stud_info = pd.read_csv(path_stud)
     name_stud = stud_info['姓名']
     num_stud = len(name_stud)
     i = np.random.randint(num_stud)
     return name_stud[i]
+
+name = pick()
+print(name)
