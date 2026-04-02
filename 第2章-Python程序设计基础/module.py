35307a8f03b396234f864a675c43a31ee8c74856
xinluo2018 <xinluo_xin@163.com>
2026-04-02 13:37:53 +0800

    new commit

--- 第2章-Python程序设计基础/module.py
+++ 第2章-Python程序设计基础/module.py
@@ -6,3 +6,15 @@ def num_higher_(s, s_high=90):
     if score >= s_high:
       num=num+1
   return num 
+
+### 自动统计出超出某分数的科目数。
+def num_lower_(s, s_low=60):
+  num = 0
+  for score in s:
+    # print(score)
+    if score < s_low:
+      num=num+1
+  return num 
+
+
+
