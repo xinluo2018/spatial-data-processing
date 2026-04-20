53a054275de429f7ef0bba21029c6acf321d7cb0
xinluo2018 <xinluo_xin@163.com>
2026-04-20 11:45:36 +0800

    new commit

--- pick.py
+++ pick.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 def pick():
-    path_stud = 'students/students_2025_hometown.csv'
+    path_stud = '课程考核记录/2026春/students_hometown_2026.csv'
     stud_info = pd.read_csv(path_stud)
     name_stud = stud_info['姓名']
     num_stud = len(name_stud)
